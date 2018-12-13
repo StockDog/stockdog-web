3eff9e1d9f0b0f657aaf896b5e8d322dec6ed0e6
asnewman <ashznewman@gmail.com>
2018-12-12 22:20:38 -0800

    Clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { CookiesProvider } from 'react-cookie';
-import { Provider as AlertProvider } from 'react-alert'
 
 // Alert optional cofiguration
 const options = {
