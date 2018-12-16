d8e9f544331ff4c55c41c45fa4bbfbb010692efe
asnewman <ashznewman@gmail.com>
2018-12-16 00:16:44 -0800

    Added Stock page (unauthenticated for now)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-   BrowserRouter as Router
+  BrowserRouter as Router
 } from 'react-router-dom';
 import AuthenticatedRoute from "components/Routing/AuthenticatedRoute";
 import UnauthenticatedRoute from "components/Routing/UnauthenticatedRoute";
@@ -9,23 +9,27 @@ import { Provider } from 'react-redux';
 import store from "./store/store";
 
 // Containers
+import Stock from './containers/Stock/Stock';
 
 class App extends Component {
-   render() {
-      return (
-        <Provider store={store}>
-           <Router>
-              <div className="App">
-                
-                 {/*<UnauthenticatedRoute exact path="/" component={Login}
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+
+            <UnauthenticatedRoute path="/stock/:ticker" component={Stock}
+              appProps={this.props} />
+
+              {/*<UnauthenticatedRoute exact path="/" component={Login}
                     appProps={this.props}/>
                  <AuthenticatedRoute exact path="/portfolio" component={Portfolio}
                     appProps={this.props}/>*/}
               </div>
            </Router>
          </Provider>
-      );
-   }
-}
-
-export default withCookies(App);
+        );
+     }
+  }
+  
+  export default withCookies(App);
